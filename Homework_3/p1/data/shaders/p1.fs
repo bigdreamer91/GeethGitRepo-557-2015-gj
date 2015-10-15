62d72911521fb53ef244a4b1fece65a955a31f1f
Geethanjali Jeevanatham <geethanjali.jeev@gmail.com>
2015-10-14 22:56:55 -0500

    Assignment 3 commit

---
+++ Homework_3/p1/data/shaders/p1.fs
@@ -0,0 +1,11 @@
+//  Team members - Jiale Feng, Geethanjali Jeevanatham, Chloe McPherson
+#version 410 core                                                 
+
+
+in vec3 pass_Color;                                                 
+out vec4 color;   
+
+void main(void)                                                   
+{                                                                 
+    color = vec4(pass_Color, 1.0);                               
+}                           
---
+++ Homework_3/p2/HCI557Datatypes.cpp
@@ -0,0 +1,2 @@
+//  Team members - Jiale Feng, Geethanjali Jeevanatham, Chloe McPherson
+#include "HCI557Datatypes.h"
